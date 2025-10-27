1b292c6505c8c9bbf1413945495cde5a44d8dfe1
matiasmartin <matiass.lean@gmail.com>
2025-10-27 17:07:11 +0100

    refactor: mv function add

--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -2,10 +2,7 @@ package repo
 
 import (
 	"fmt"
-	"path/filepath"
 
-	"github.com/matiasmartin00/arbor/internal/index"
-	"github.com/matiasmartin00/arbor/internal/object"
 	"github.com/matiasmartin00/arbor/internal/utils"
 )
 
@@ -22,43 +19,6 @@ func Init(path string) error {
 	}
 	headPath := utils.GetHeadPath(path)
 	return utils.WriteFile(headPath, []byte("refs/heads/main"))
-}
-
-func Add(repoPath, filePath string) (string, error) {
-	absRepo, _ := filepath.Abs(repoPath)
-
-	// read working directory file content
-	content, err := utils.ReadFile(filePath)
-	if err != nil {
-		return "", err
-	}
-
-	// write object
-	hash, err := object.WriteBlob(absRepo, content)
-	if err != nil {
-		return "", err
-	}
-
-	// update index
-	idx, err := index.Load(absRepo)
-	if err != nil {
-		return "", err
-	}
-	relPath, err := filepath.Rel(absRepo, filePath)
-	if err != nil {
-		relPath = filePath
-	}
-
-	// if the file is outside the repo, use their relative path from the pwd
-	if relPath == "." || relPath == "" {
-		relPath = filepath.Base(filePath)
-	}
-	idx[relPath] = hash
-	if err := idx.Save(absRepo); err != nil {
-		return "", err
-	}
-
-	return hash, nil
 }
 
 func EnsureRepo(path string) error {
