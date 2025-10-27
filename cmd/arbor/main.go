7f2c81b4f6391b9e8fde6228284531d573c3aceb
matiasmartin <matiass.lean@gmail.com>
2025-10-27 15:01:12 +0100

    feat: basic init

--- cmd/arbor/main.go
+++ cmd/arbor/main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/matiasmartin00/arbor/internal/repo"
+)
 
 func main() {
 	fmt.Println("Arbor VCS - Command Line Interface")
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: arbor <command> [options]")
+		return
+	}
+
+	command := os.Args[1]
+	switch command {
+	case "init":
+		err := repo.Init(".")
+		if err != nil {
+			fmt.Println("Error initializing repository:", err)
+			os.Exit(1)
+		}
+		fmt.Println("Repository initialized.")
+	default:
+		fmt.Println("Unknown command:", command)
+	}
 }
--- internal/repo/repo.go
+++ internal/repo/repo.go
@@ -1,1 +1,31 @@
 package repo
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+)
+
+const repoDir = ".arbor"
+const headFile = "HEAD"
+
+var dirs = []string{
+	"objects",
+	"refs/heads",
+}
+
+func Init(path string) error {
+	fmt.Println("Initializing repository at", path)
+	basePath := filepath.Join(path, repoDir)
+	for _, dir := range dirs {
+		if err := createDir(filepath.Join(basePath, dir)); err != nil {
+			return err
+		}
+	}
+	headPath := filepath.Join(basePath, headFile)
+	return os.WriteFile(headPath, []byte("refs/heads/main"), 0644)
+}
+
+func createDir(path string) error {
+	return os.MkdirAll(path, 0755)
+}
